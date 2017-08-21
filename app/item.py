12e63300c0f0179de67237a200bd296cb063a224
flacode <flavianshemerirwe@gmail.com>
2017-08-21 21:44:06 +0300

    FEATURES: Add the remaining CRUD features

--- app/item.py
+++ app/item.py
@@ -1,6 +1,6 @@
 class Item(object):
     """Class for the Items in a shopping list"""
-    def __init__(self, name, quantity, bought_from, status=None):
+    def __init__(self, name, quantity, bought_from=None, status=None):
         self.name = name
         self.quantity = quantity
         self.bought_from = bought_from
